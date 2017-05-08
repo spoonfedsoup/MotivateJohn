fff2d4f7253d3fd41c85a1030ded51d9e8167826
spoonfedsoup <spoonfedsoup@gmail.com>
2017-05-08 00:32:19 -0400

    first commit

---
+++ bot.js
@@ -0,0 +1,10 @@
+var Twit = require('twit')
+var express = require('express')
+var config = require('./config.js')
+
+var T = new Twit({
+  consumer_key:
+  consumer_secret:
+  access_token:
+  access_token_secret:
+})
