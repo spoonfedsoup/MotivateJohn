b56528512751ef32712538d325fc00b83d26965d
spoonfedsoup <spoonfedsoup@gmail.com>
2017-05-21 14:40:12 -0400

    record new tweets. playing with json

--- bot.js
+++ bot.js
@@ -1,10 +1,15 @@
-var Twit = require('twit')
-var express = require('express')
-var config = require('./config.js')
+var Twit = require('twit');
+var express = require('express');
+var config = require('./config.js');
+var fs = require('fs');
 
-var T = new Twit({
-  consumer_key:
-  consumer_secret:
-  access_token:
-  access_token_secret:
-})
+var t = new Twit(config)
+
+fs.writeFile("tweets","seting up \n",function(err){});
+
+var stream = t.stream('statuses/filter', {track:'#philly'})
+console.log("Listening");
+stream.on('tweet',function(tweet){
+  console.log(tweet);
+  fs.appendFile("tweets",JSON.stringify(tweet),function(err){});
+ });
