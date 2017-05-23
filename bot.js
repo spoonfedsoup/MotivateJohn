1034ec271c72842cee8a92945cf13d97a6501d12
spoonfedsoup <spoonfedsoup@gmail.com>
2017-05-22 23:28:04 -0400

    Messing around with storing JSON objects. Probably not the right direction to go

--- bot.js
+++ bot.js
@@ -2,16 +2,19 @@ var Twit = require('twit');
 var express = require('express');
 var config = require('./config.js');
 var fs = require('fs');
+var tweets = require('./tweets.JSON');
 
 var t = new Twit(config)
+var i = 0;
 
-fs.writeFile("tweets","seting up \n",function(err){});
 
 var stream = t.stream('statuses/filter', {track:'#philly'})
 console.log("Listening");
 stream.on('tweet',function(tweet){
-  console.log(tweet);
-  fs.appendFile("tweets",stringit(tweet),function(err){});
+  console.log("Tweet recieved");
+  tweets[i] = tweet;
+  i++;
+  fs.writeFile("tweets.JSON",tweets);
  });
 
 function stringit(tweet){
